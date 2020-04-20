8d2df290ba44c0ae01a523cf8d6158e74230076f
Dylan Palmieri <dgpalmieri@gmail.com>
2020-04-20 14:14:51 -0800

    Add Hello, World

---
+++ shell.go
@@ -0,0 +1,15 @@
+// shell.go
+// Dylan Palmieri
+// 4/20/20
+// Custom shell for CS321 Operating Systems
+
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("Hello, world!")
+
+}
