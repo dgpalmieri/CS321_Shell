f98f273dec1b53b7d645d8f9b3d8ce621d24218c
Dylan Palmieri <dgpalmieri@gmail.com>
2020-04-20 17:45:15 -0800

    Add basic input parsing structure

--- shell.go
+++ shell.go
@@ -6,10 +6,38 @@
 package main
 
 import (
+    "bufio"
     "fmt"
+    "os"
 )
 
-func main() {
-    fmt.Println("Hello, world!")
+func parseInput(input string) (bool, bool) {
+    var err, exit = false, false
 
+    if (input == "exit\n"){
+        exit = true
+    }
+
+    return err, exit
+}
+
+func main() {
+    reader := bufio.NewReader(os.Stdin)
+
+    for true {
+        fmt.Println("----- /home/dgpalmieri/Documents/Operating_Systems/Project_Shell ------")
+        fmt.Print("Hello, user: ")
+        input, _ := reader.ReadString('\n')
+        err, exit := parseInput(input)
+
+        if err {
+            fmt.Println("Unable to parse input.")
+        }
+
+        if exit {
+            break
+        }
+    }
+
+    fmt.Println("Exiting.")
 }
